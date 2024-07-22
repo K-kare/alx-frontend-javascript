c89c652dd6b5de8b354a4f47d6a56d0223f41a60
K-kare <kamunawangari@gmail.com>
2024-07-22 12:41:51 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,9 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-     task = true;
-     task2 = false;
+ if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
