4ba3aea1d973ef1b1f00fd9cacb3cd771d0d31d1
K-kare <kamunawangari@gmail.com>
2024-07-24 11:23:01 +0000

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) return new Promise(resolve) => { status: 200, body: 'Success' });
-  return new Promise.reject(new Error('The fake API is not working currently'));
+  if (true) return Promise.resolve({ status: 200, body: 'Success' });
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
