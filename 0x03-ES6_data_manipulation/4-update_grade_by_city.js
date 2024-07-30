b70bf28fcd0912ad16712b5062d7077eeee21ef2
K-kare <kamunawangari@gmail.com>
2024-07-30 19:29:32 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(listStudents, newLocation, newGrades) {
   return listStudents
-    .filter(({ location }) => location === targetLocation)
+    .filter(({ location }) => location === newLocation)
     .map((obj) => {
       const targetNewGrade = newGrades.filter(({ studentId }) => studentId === obj.id);
       const { grade = 'N/A' } = targetNewGrade.length > 0 ? targetNewGrade[0] : {};
