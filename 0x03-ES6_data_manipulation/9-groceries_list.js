989267c32cb57ceef11a557073fcc84f412c2a5d
K-kare <kamunawangari@gmail.com>
2024-08-02 20:57:36 +0000

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-  const values = [
+  const data = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-  return new Map(values);
+  return new Map(data);
 }
